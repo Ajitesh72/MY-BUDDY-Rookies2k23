eede447ecf12b0511193b7dbcac3205d002cd19b
Ajitesh <Ajitesh72>
2023-02-05 02:00:00 +0530

    Added Footer

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -1,10 +1,14 @@
 import "../styles/Contactus.css";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
+import Footer from "../components/Footer";
 
 function ContactUs() {
   return (
     <div>
-        ContactUs
+      <div>               
+      {/* this div/component will be present in the botton of pages */}
+        <Footer />    
+      </div>
     </div>
   );
 }
